ddb05cea8d5bb0675ca7c31fb71736a3cb6bc4bb
Lsong <song940@gmail.com>
2021-02-25 18:54:43 +0800

    add build form

--- index.js
+++ index.js
@@ -93,6 +93,23 @@ const tcp = (host, port) => {
   });
 };
 
+const buildForm = body => {
+  const output = [];
+  const uuid = (Date.now() + (Math.random() * 1e8 | 0)).toString(36);
+  const boundary = `----WebKitFormBoundary${uuid}`;
+  for (const key of Object.keys(body)) {
+    const value = body[key];
+    output.push("--" + boundary);
+    output.push(`Content-Disposition: form-data; name="${key}"`);
+    output.push('');
+    output.push(value);
+  }
+  output.push('--' + boundary + '--');
+  output.push('');
+  const payload = output.join('\r\n');
+  return { boundary, payload };
+};
+
 module.exports = {
   tcp,
   request,
@@ -103,4 +120,5 @@ module.exports = {
   cookieJar,
   stringify,
   getJSON,
+  buildForm,
 };
