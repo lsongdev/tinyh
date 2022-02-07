3629a725d6053bbec1a422d28a5947c004cafdbd
Lsong <song940@gmail.com>
2022-02-07 17:05:10 +0800

    update

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const net = require('net');
 const http = require('http');
 const https = require('https');
 const assert = require('assert');
@@ -20,12 +19,6 @@ const request = (method, url, payload, headers) => {
   });
 };
 
-const get = (url, headers) =>
-  request('get', url, '', headers);
-
-const post = (url, payload, headers) =>
-  request('post', url, payload, headers);
-
 const readStream = stream => {
   const buffer = [];
   return new Promise((resolve, reject) => {
@@ -35,13 +28,6 @@ const readStream = stream => {
       .on('end', () => resolve(Buffer.concat(buffer)))
   });
 };
-
-const getJSON = (url, headers) =>
-  Promise
-    .resolve()
-    .then(() => get(url, headers))
-    .then(readStream)
-    .then(JSON.parse);
 
 const ensureStatusCode = expected => {
   if (!Array.isArray(expected))
@@ -53,72 +39,32 @@ const ensureStatusCode = expected => {
   };
 };
 
-const cookieJar = (cookies = {}) => {
-  return {
-    cookies,
-    get cookie() {
-      return Object
-        .keys(cookies)
-        .map(key => `${key}=${cookies[key]}`).join('; ');
-    },
-    setCookie(res) {
-      (res.headers['set-cookie'] || []).forEach(a => {
-        const [b] = a.split(/;\s?/);
-        const [k, v] = b.split('=');
-        cookies[k] = v;
-      });
-      return res;
-    },
-    save() {
+const get = (url, headers) =>
+  request('get', url, '', headers);
 
-    },
-    restore() {
+const post = (url, payload, headers) =>
+  request('post', url, payload, headers);
 
-    }
-  };
-};
+const getJSON = (url, headers) =>
+  Promise
+    .resolve()
+    .then(() => get(url, headers))
+    .then(readStream)
+    .then(JSON.parse);
 
-const stringify = (obj, { encode = encodeURIComponent } = {}) =>
-  Object
-    .keys(obj)
-    .reduce((arr, key) => {
-      const value = obj[key];
-      arr.push(encodeURIComponent(key) + `=${encode(value)}`);
-      return arr;
-    }, []).join('&');
 
-const tcp = (host, port) => {
-  return new Promise(resolve => {
-    const socket = net.createConnection({ host, port }, () => resolve(socket));
-  });
-};
-
-const buildForm = body => {
-  const output = [];
-  const uuid = (Date.now() + (Math.random() * 1e8 | 0)).toString(36);
-  const boundary = `----WebKitFormBoundary${uuid}`;
-  for (const key of Object.keys(body)) {
-    const value = body[key];
-    output.push("--" + boundary);
-    output.push(`Content-Disposition: form-data; name="${key}"`);
-    output.push('');
-    output.push(value);
-  }
-  output.push('--' + boundary + '--');
-  output.push('');
-  const payload = output.join('\r\n');
-  return { boundary, payload };
-};
+const postJSON = (url, payload, headers) =>
+  Promise
+    .resolve()
+    .then(() => post(url, JSON.stringify(payload), headers))
+    .then(readStream)
 
 module.exports = {
-  tcp,
   request,
   get,
   post,
   readStream,
   ensureStatusCode,
-  cookieJar,
-  stringify,
   getJSON,
-  buildForm,
+  postJSON,
 };
